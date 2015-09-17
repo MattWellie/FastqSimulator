d3479d247f66030c7461f503a3115bb23967e7c1
Matt Welland <mattwellie@gmail.com>
2015-09-17 16:02:09 +0100

    Update sequence_modifier.py

--- sequence_modifier.py
+++ sequence_modifier.py
@@ -1,16 +1,14 @@
-"""
-This class creates single nucleotide changes in each exon of the input sequence. The exons are located using the exon
-coordinates from the input file. Random is used to choose a random location within the exon and a new base. The base
-substitution is made and the position is recorded in a dictionary object
-"""
-
+import random
 __author__ = 'mwelland'
 
 
-import random
-
-
 class Modifier:
+
+    """ This class creates single nucleotide changes in each exon of the input sequence
+        The exons are located using the exon coordinates from the input file
+        Random is used to choose a random location within the exon and a new base
+        The base substitution is made and the position is recorded in a dict
+    """
 
     def __init__(self, file_dict, type):
         self.type = type
@@ -30,8 +28,7 @@ class Modifier:
         for transcript in self.dict['transcripts']:
             self.transcript_pos = 0
             self.transcript_dict = self.dict['transcripts'][transcript]
-            self.output_dict[transcript] = {'variants': {},
-                                            'exons': {}}
+            self.output_dict[transcript] = {'variants': {}, 'exons': {}}
             exon_list = self.transcript_dict['list_of_exons']
             self.output_dict[transcript]['exon list'] = exon_list
             for exon in exon_list:
@@ -46,14 +43,12 @@ class Modifier:
             for exon in exon_list:
                 start = self.transcript_dict['exons'][exon]['genomic_start']
                 end = self.transcript_dict['exons'][exon]['genomic_end']
-                exon_seq = self.genomic[start - self.padding:end + self.padding]
-                self.output_dict[transcript]['exons'][exon] = {
-                    'start': start,
-                    'end': end,
-                    'padded seq': exon_seq,
-                    'padded length': len(exon_seq),
-                    'length': end - start
-                }
+                exon_seq = self.genomic[start - self.padding: end + self.padding]
+                self.output_dict[transcript]['exons'][exon] = {'start': start,
+                                                               'end': end,
+                                                               'padded seq': exon_seq,
+                                                               'padded length': len(exon_seq),
+                                                               'length': end - start}
         return self.output_dict
 
     def modify(self, exon_number, transcript, start, end, b_or_a, old_cds):
@@ -77,15 +72,14 @@ class Modifier:
             cds_delay = old_cds
         else:
             cds_delay = self.transcript_dict['cds_offset']
-        p_length = self.transcript_dict['protein_length'] + 3  # len(stop codon)
+        p_length = self.transcript_dict['protein_length'] + 3  # For stop codon length
         exon_length = end - start
         """
         Put in something here to deal with if the exon is pre-coding
         b_or_a is 'before' if the exon is pre-coding sequence
         """
-        # Edit coord + 1 to compensate for 0-base counting sequence index
         if b_or_a == 'before':
-            hgvs_pos = (edit_coord + 1) - (old_cds - 1)
+            hgvs_pos = (edit_coord) - (old_cds - 1)  # Trying to fix indexing
         else:
             variant_pos = (edit_coord + 1) - start
             hgvs_pos = (self.transcript_pos + variant_pos) - cds_delay
@@ -93,7 +87,9 @@ class Modifier:
         """
         This is the correct hgvs nomenclature for annotation by annovar
         """
-        if hgvs_pos <= 0:
+        if hgvs_pos <= 0 and self.type == 'lrg':
+            hgvs = 'c.%d%s>%s' % (hgvs_pos, old_base, base)
+        elif hgvs_pos <= 0:
             hgvs = 'c.%d%s>%s' % (hgvs_pos - 1, old_base, base)
         elif hgvs_pos > p_length:
             after_coding = hgvs_pos - p_length  # compensate for no 0s
@@ -106,8 +102,7 @@ class Modifier:
         # Un comment this section for positional debugging
         print 'start: %d, end: %d' % (start, end)
         print 'Length: %d' % (exon_length)
-        print 'for gene: %s, file: %s cds: %d' % (self.dict['genename'],
-         self.dict['filename'], cds_delay)
+        print 'for gene: %s, file: %s cds: %d' % (self.dict['genename'], self.dict['filename'], cds_delay)
         print 'position %d in exon %d' % (variant_pos, exon_number)
         print 'changed %s to %s' % (old_base, base)
         print 'HGVS: %s' % hgvs
@@ -130,9 +125,7 @@ class Modifier:
                 break
         this = raw_input()
         """
-        self.output_dict[transcript]['variants'][exon_number] = {
-            'position': edit_coord,
-            'new base': base,
-            'hgvs': hgvs,
-            'transcript': self.transcript_dict['NM_number']
-        }
+        self.output_dict[transcript]['variants'][exon_number] = {'position': edit_coord,
+                                                                 'new base': base,
+                                                                 'hgvs': hgvs,
+                                                                 'transcript': self.transcript_dict['NM_number']}
