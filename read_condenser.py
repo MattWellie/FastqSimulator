b48d12a4a1168ef430cc3b9e49db76965b47a8f7
Matt Welland <mattwellie@gmail.com>
2015-09-17 16:03:55 +0100

    Update read_condenser.py

--- read_condenser.py
+++ read_condenser.py
@@ -1,18 +1,17 @@
-"""
-A class which will be used to take the outputs of several fastQ files with different contents and condense them into a
-format ready for alignment
-"""
+import os
 
 __author__ = 'mwelland'
 
 
-import os
-
-
 class Condenser:
 
-    def __init__(self, gene_list):
+    """ A class which will be used to take the outputs of several fastQ files with different contents and condense
+        them into a format ready for alignment
+    """
+
+    def __init__(self, gene_list, run_number):
         self.gene_list = gene_list
+        self.run_number = run_number
         self.file_list = os.listdir('fastQs')
         self.gene_dictionary = {}
 
@@ -32,22 +31,19 @@ class Condenser:
 
     def populate_dict(self, gene, name_length):
         """
-        The dictionary will contain the names of all files relating to the selected gene. This method uses the gene name
-        and the transcript numbers to identify the unchanged sequence and the accompanying changed transcripts. This is
-        done to allow each separate transcript to be paired with the unchanged sequence, so as to represent a homozygous
-         variant as well as improving the read depth for each area.
+        The dictionary will contain the names of all files relating to the selected gene. This method uses the gene
+        name and the transcript numbers to identify the unchanged sequence and the accompanying changed transcripts.
+        This is done to allow each separate transcript to be paired with the unchanged sequence, so as to represent
+        a homozygous variant as well as improving the read depth for each area.
         :param gene:
         :param name_length:
         :return:
         """
-        self.gene_dictionary = {1: {'ref': '',
-                                    'transcripts': []},
-                                2: {'ref': '',
-                                    'transcripts': []}}
+        self.gene_dictionary = {1: {'ref': '', 'transcripts': []}, 2: {'ref': '', 'transcripts': []}}
         read1s = []
         read2s = []
-        fq_list = [x for x in self.file_list if x[:name_length] == gene]
-        # Separate read 1s from read 2s
+        fq_list = [name for name in self.file_list if name[:name_length] == gene]
+        #  Separate read 1s from read 2s
         for filename in fq_list:
             first_part_of_name = filename.split('.')[0]
             if first_part_of_name[-1] == '1':
@@ -83,17 +79,13 @@ class Condenser:
         file_pairs = []
         read_dict = self.gene_dictionary[read]
         for filename in read_dict['transcripts']:
-            file_pairs.append([read_dict['ref'],
-                               filename,
-                               self.create_file_name(gene, name_length,
-                                                     filename, read)])
+            file_pairs.append([read_dict['ref'], filename, self.create_file_name(gene, name_length, filename, read)])
         return file_pairs
 
     @staticmethod
     def create_file_name(gene, name_length, filename, read):
         """
-        Creates a new file name which combines the gene name, the transcript
-        accession number and the read number
+        Creates a new file name which combines the gene name, the transcript number and the read number
         """
         transcript = filename[name_length:name_length+1]
         filename = '%s_transcript%s_R%d.fq' % (gene, transcript, read)
@@ -102,9 +94,8 @@ class Condenser:
     @staticmethod
     def combine_files(read_pairs):
         """
-        This will combine the created files in memory and write the output to
-        a new file; the filename for the new file will be the one created in
-        create_file_name()
+        This will combine the created files in memory and write the output to a new file
+        the filename for the new file will be the one created in create_file_name
         :param read_pairs:
         :return:
         """
@@ -115,15 +106,11 @@ class Condenser:
 
             with open(os.path.join('fastQs', outfile_name), 'w') as output_file:
                 reference_file_name = triple_tuple[0]
-                reference_file = open(os.path.join('fastQs',
-                                                   reference_file_name
-                                                   ), "r")
+                reference_file = open(os.path.join('fastQs', reference_file_name), "r")
                 contents = reference_file.readlines()
                 output_file.writelines(contents)
                 alt_file = triple_tuple[1]
-                contents = open(os.path.join('fastQs',
-                                             alt_file
-                                             ), 'r').readlines()
+                contents = open(os.path.join('fastQs', alt_file), 'r').readlines()
                 output_file.writelines(contents)
 
     def erase_old_files(self):
